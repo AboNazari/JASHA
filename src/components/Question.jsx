2aa0b324b935845e38a94c66548d1d3701895d3c
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-02-25 17:39:21 +0600

    FAQ editet

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,8 +5,13 @@ import { ReactComponent as Down } from "../icons/Down.svg";
 const Question = ({ Question, Answer, open }) => {
   return (
     <div>
-      <li className="border-t-2 border-dark py-4 flex spcae-x-10">
-        <p>{Question}</p> {open ? <Up /> : <Down />}
+      <li className="border-t-2 border-dark py-4 flex  justify-between text-lg px-5 items-center">
+        <p>{Question}</p>{" "}
+        {open ? (
+          <Up className="hover:text-2xl" />
+        ) : (
+          <Down className=" transition transform hover:translate-y-2 hover:duration-[400ms] ease-out hover:ease-in " />
+        )}
       </li>
       {open ? <p className="p-10 mt-4 text-xl font-primary">{Answer}</p> : ""}
     </div>
