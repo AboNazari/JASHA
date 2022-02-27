32871f67cf0da69b6be83500b3212992ca863209
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-02-27 22:55:55 +0600

    FAQ fixed

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Up } from "../icons/Up.svg";
 import { ReactComponent as Down } from "../icons/Down.svg";
 
-const Question = ({ Question, Answer, open, openHandler, id }) => {
+const Question = ({ Question, Answer, id }) => {
+  const [active, setActive] = useState(false);
+
+  const openTab = () => {
+    setActive(!active);
+  };
+
   return (
     <div>
       <li
@@ -10,19 +16,19 @@ const Question = ({ Question, Answer, open, openHandler, id }) => {
         className="border-t-2 border-dark py-4 flex  justify-between text-lg px-5 items-center"
       >
         <p>{Question}</p>
-        {open ? (
+        {active ? (
           <Up
             className=" transition transform hover:-translate-y-1 hover:duration-[400ms] ease-out hover:ease-in"
-            onClick={openHandler}
+            onClick={openTab}
           />
         ) : (
           <Down
             className=" transition transform hover:translate-y-1 hover:duration-[400ms] ease-out hover:ease-in "
-            onClick={openHandler}
+            onClick={openTab}
           />
         )}
       </li>
-      {open ? (
+      {active ? (
         <p className="md:p-5 p-3  text-xl font-primary first-letter:pl-6">
           {Answer}
         </p>
--- src/layouts/FAQ.jsx
+++ src/layouts/FAQ.jsx
@@ -5,23 +5,6 @@ import QuestionsData from "../data/QuestionsData";
 
 const FAQ = () => {
   const [data, setData] = useState(QuestionsData);
-  const [isOpen, setIsOpen] = useState(false);
-
-  // The Open Handler
-  const onToOpenClick = (e) => {
-    console.log(e.target.parentElement.id);
-
-    const id = e.target.parentElement.id;
-
-    console.log(data.id);
-
-    // Check out the existence of the id
-    data.map((Q) => {
-      if (Q.id === id) {
-        setIsOpen(!isOpen);
-      }
-    });
-  };
 
   return (
     <div className="w-11/12 lg:w-10/12 rounded-xl shadow-lg  mx-auto p-10 ">
@@ -37,8 +20,6 @@ const FAQ = () => {
                 id={Que.id}
                 Question={Que.Q}
                 Answer={Que.A}
-                open={Que.isOpen}
-                openHandler={onToOpenClick}
               />
             );
           })}
