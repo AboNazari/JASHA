1c18bff29741b7af6b67cb8eca4be4f1b4f913d1
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-04-12 16:22:22 +0600

    the FAQ page is updated

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -13,25 +13,23 @@ const Question = ({ Question, Answer, id }) => {
     <div>
       <li
         id={id}
-        className="border-t-2 border-dark py-4 flex  justify-between text-lg px-5 items-center"
+        className="border-t-2 border-dark py-4 flex  justify-between text-lg md:px-5 px-2 items-center"
       >
         <p>{Question}</p>
         {active ? (
           <Up
-            className=" transition transform hover:-translate-y-1 hover:duration-[400ms] ease-out hover:ease-in"
+            className=" transition transform hover:-translate-y-1 hover:duration-[400ms] ease-out hover:ease-in max-w-[20px]"
             onClick={openTab}
           />
         ) : (
           <Down
-            className=" transition transform hover:translate-y-1 hover:duration-[400ms] ease-out hover:ease-in "
+            className=" transition transform hover:translate-y-1 hover:duration-[400ms] ease-out hover:ease-in max-w-[20px]"
             onClick={openTab}
           />
         )}
       </li>
       {active ? (
-        <p className="md:p-5 p-3  text-xl font-primary first-letter:pl-6">
-          {Answer}
-        </p>
+        <p className="md:p-5 p-3  text-xl font-primary ">{Answer}</p>
       ) : (
         ""
       )}
