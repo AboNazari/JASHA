b18045ec4b3ffe2d27e5ec998457a7d0f3fe3219
Zakir Hussain Muradi <zakir.hussainmuradi_2025@ucentralasia.org>
2022-05-03 20:21:19 +0600

    Updated: About.jsx completed successfully

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { ReactComponent as Girl } from "../images/girlpen.svg";
 
 const About = () => {
   return (
-    <div className="w-full h-full lg:h-screen overflow-hidden flex flex-col md:flex-row md:items-center p-10 md:pr-0">
-      <div>
-        <span></span>
-        <h2>ABOUT US</h2>
-        <button>Jasha - Live</button>
+    <div className="w-5/6 h-full lg:h-screen overflow-hidden flex flex-col p-10 md:pr-0 m-auto">
+      <div className="w-full relative">
+        <div className=" bg-primary w-[11rem] h-[6px]"></div>
+        <h2 className="text-5xl mt-6 tracking-widest">ABOUT US</h2>
+        <button className="px-16 text-white font-bold text-xl py-4 bg-primary mt-16 rounded-xl absolute right-0">
+          Jasha - Live
+        </button>
       </div>
-      <div>
-        <img src="#" alt="" />
-        <p>
+      <div className="w-full mt-40 px-4  bg-gradient-to-r from-white to-[#CED6CF] flex rounded-2xl">
+        <Girl className="w-1/2" />
+        <p className="w-1/2 p-4 text-lg font-[Roboto] pt-32 leading-10">
           “Jasha” is a social media platform for public exchange of posts
           (texts, photos, videos, music) aimed at publishing notes in the form
           of blogs and articles about conscious living. In Jasha, all posts
           focus on topics around conscious living: eco-friendly life style,
-          mental and physical wellbeing, and personal growth. Jasha welcomes
-          everyone, regardless of race, age, ethnicity, religion, gender, sexual
-          identity, or any other affiliation.
+          mental and physical wellbeing, and personal growth.
+          <br />
+          <br />
+          Jasha welcomes everyone, regardless of race, age, ethnicity, religion,
+          gender, sexual identity, or any other affiliation.
         </p>
       </div>
     </div>
