26f498eeea5f183c03b756e346b763520c87b228
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-05-06 20:51:26 +0600

    udpated: component tree

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -3,7 +3,7 @@ import PlaneAnim from "../animations/plane";
 
 const Join = ({ setShow }) => {
   return (
-    <div className="fixed flex justify-center items-center flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 bg-gray-200 h-[40vw] rounded-[4rem] px-10">
+    <div className="fixed flex justify-center items-center flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/5 bg-white shadow-xl h-[22vw] rounded-[2rem] px-10 z-50">
       <h1 className="text-5xl">Join our cause</h1>
       <div
         onClick={() => setShow(false)}
@@ -14,17 +14,17 @@ const Join = ({ setShow }) => {
       <div className="grid grid-cols-5 gap-4 w-full">
         <div className="col-span-3 grid grid-cols-2 gap-5 items-center content-center">
           <input
-            className="col-span-1 px-7 py-6 rounded-lg"
+            className="col-span-1 px-7 py-6 rounded-lg border-4"
             type="text"
             placeholder="Name"
           />
           <input
-            className="col-span-1 px-7 py-6 rounded-lg"
+            className="col-span-1 px-7 py-6 rounded-lg border-4"
             type="text"
             placeholder="Country"
           />
           <input
-            className="col-span-2 px-7 py-6 rounded-lg"
+            className="col-span-2 px-7 py-6 rounded-lg border-4"
             type="text"
             placeholder="E-mail"
           />
--- src/layouts/Quote.jsx
+++ src/layouts/Quote.jsx
@@ -6,10 +6,10 @@ const Quote = () => {
     <div className="w-full text-black lg:mb-[10rem]">
       <div className="lg:w-3/4 px-4 mt-10 m-auto text-dark mb-20 ">
         <div className="flex flex-col w-[10rem]">
-          <Hat className="m-auto" />
+          <Hat className="m-auto fill-primary" />
           <div className=" bg-primary w-[11rem] h-[6px] mt-4"></div>
         </div>
-        <h3 className="text-5xl tracking-wider pt-7">Quote OF THE DAY</h3>
+        <h3 className="text-5xl tracking-wider pt-7">QUOTE OF THE DAY</h3>
       </div>
       <div className="flex gap-8 lg:w-2/4 mx-2  lg:m-auto flex-col  bg-gradient-to-r from-white bg-[#CED6CF] shadow-md px-5 py-10 md:p-10 rounded-3xl ">
         <h3 className="font-primary text-2xl text-dark">Mahatma Gandhi</h3>
