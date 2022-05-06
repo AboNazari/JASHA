ff662ef421f33ba8276829d89fbfe7fbf7f92ce3
Zakir Hussain Muradi <zakir.hussainmuradi_2025@ucentralasia.org>
2022-05-06 17:05:05 +0600

    Updated: Responsived quote page

--- src/layouts/Quote.jsx
+++ src/layouts/Quote.jsx
@@ -4,20 +4,20 @@ import { ReactComponent as Hat } from "../icons/hat.svg";
 const Quote = () => {
   return (
     <div className="w-full text-black lg:mb-[10rem]">
-      <div className="w-3/4 m-auto text-dark mb-20">
+      <div className="lg:w-3/4 px-4 mt-10 m-auto text-dark mb-20 ">
         <div className="flex flex-col w-[10rem]">
           <Hat className="m-auto" />
           <div className=" bg-primary w-[11rem] h-[6px] mt-4"></div>
         </div>
         <h3 className="text-5xl tracking-wider pt-7">Quote OF THE DAY</h3>
       </div>
-      <div className="flex gap-8 w-2/4 m-auto flex-col  bg-gradient-to-r from-white bg-[#CED6CF] shadow-md  p-10 rounded-3xl ">
+      <div className="flex gap-8 lg:w-2/4 mx-2  lg:m-auto flex-col  bg-gradient-to-r from-white bg-[#CED6CF] shadow-md px-5 py-10 md:p-10 rounded-3xl ">
         <h3 className="font-primary text-2xl text-dark">Mahatma Gandhi</h3>
         <p className="text-3xl leading-loose font-quote tracking-wider">
           “You will never know what results <br /> come of your actions, but if
           you do nothing, <br /> there will be no results.”
         </p>
-        <p className=" text-right font-bold text-lg text-black">
+        <p className=" text-right font-bold lg:text-lg text-black">
           6th May, 2022
         </p>
       </div>
